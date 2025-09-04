eeb368b5ff87a8da43b90857f1175aae8be92160
SamuelPereZz <fernando.samuelperez01@gmail.com>
2025-09-03 23:50:54 -0600

    variables y tipo de datos

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# 21-dias-de-choque
+# 21 días de choque en Javascript.
+### Este repositorio es un repaso de 21 días para aprender lo basico de lenguaje Javascript.
+
+## 1.- Tipos de datos.
+
+En Javascript tenemos 9 tipos de datos divididos en 2 grupos conocidos como los primitivos y los NO primitivos.
+Dentro de los tipos primitivos tenemos 7 tipos de datos:
+
+* number 
+* string
+* boolean
+* null
+* undefined
+* symbol
+* bigint
---
+++ javascript.js
@@ -0,0 +1,23 @@
+console.log('Sam Empieza hoy, cero excusas');
+
+var DatoNumerico = 5;
+var DatoNumerico2 = 10.12;
+DatoNumerico + DatoNumerico2; //15.12
+//En JavaScript, no hay una diferencia entre números enteros y números decimales, todos los números son de tipo number
+
+const DatoCadena = 'Hola mundo!';
+const DatoCadena2 = `Esto es una cadena de texto
+que ocupa varias líneas. Puedes escribir
+tantas líneas como quieras`;
+//La cadena de texto (tipo string) es otro tipo de dato muy común. En JavaScript, las cadenas de texto se representan entre comillas simples, dobles o acentos graves Las comillas simples y dobles funcionan igual, pero al usar acentos graves podemos escribir cadenas de texto que ocupen varias líneas:
+let DatoBoleano = true;
+//Los booleanos representa sólo dos valores: true (verdadero) o false (falso). Por ejemplo:
+//¿La luz está encendida (true) o apagada (false)?
+//¿Está lloviendo (true) o no está lloviendo (false)?
+//¿Está el usuario logueado (true) o no está logueado (false)?
+
+var DatoNull = null;
+const DatoSinDefinir = undefined;
+let DatoSimbolo = Symbol;
+
+var DatoBigInt = BigInt;
