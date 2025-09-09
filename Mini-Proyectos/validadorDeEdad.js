702af0486e8e3fe2297d0381ded59acc440a3c14
SamuelPereZz <fernando.samuelperez01@gmail.com>
2025-09-08 23:42:18 -0600

    Proyecto de validacion de edad completado

--- Mini-Proyectos/validadorDeEdad.js
+++ Mini-Proyectos/validadorDeEdad.js
@@ -1,10 +1,16 @@
 const miFormulario = document.getElementById('validarEdad');
 
-miFormulario.addEventListener('submit', function(event) {
+miFormulario.addEventListener('submit', function (event) {
   // 1. Prevenir el envío automático del formulario
   event.preventDefault();
 
   const formData = new FormData(this);
   const edad = formData.get('edad');
   console.log(edad);
+
+  if (edad < 18) {
+    alert('Eres menor');
+  } else {
+    alert('Bienvenido');
+  }
 });
