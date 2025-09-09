6ccdff0ceb4199e3830b396d97cb3bdb829a3569
SamuelPereZz <fernando.samuelperez01@gmail.com>
2025-09-08 23:30:11 -0600

    mini proyecto de validacion de edad. Primera version sin condicional

--- Repasos/variablesYTiposdeDatos.js
+++ Repasos/variablesYTiposdeDatos.js
@@ -28,25 +28,3 @@ let DatoSimbolo = Symbol;
 var DatoBigInt = BigInt;
 //Es un tipo de dato numérico que permite representar enteros con precisión arbitraria por ejemplo numeros infinitos como PI: 3.14159265358979323846264338327950288419716939937510582097494459230781640628620.
 
-// Dia 3
-
-let fecha = new Date();
-console.log(fecha); // Fri Sep 05 2025 00:01:08 GMT-0600 (hora estándar central)
-
-fecha.getMonth(); // 05 (retorna un número que es el día)
-fecha.getFullYear(); // 2025 (retorna un número que es el año)
-fecha.toISOString(); // '2025-09-11T22:22:33.132Z' (retorna un string)
-
-let literal = {};
-let constructora = new Object();
-//Hay dos formas de crear un nuevo objeto vacío. Usando un objeto literal o usando la función constructora de objecto
-
-let usuario = { nombre: 'Samuel', apellido: "pérez", edad: 25, altura: 173, conTrabajo: {esDesarrollador: true}  };
-//Como puedes observar, la llave es un texto y los valores pueden ser cualquier tipo de dato (inclusive otros objetos).
-//Una propiedad esta compuesta por una llave y un valor. Si creas más de una propiedad, deberás separar cada propiedad con una coma (,):
-
-// Notación de punto `.`
-console.log(usuario.conTrabajo.esDesarrollador); // True
-// Notación de corchetes `[]`
-console.log(usuario["nombre"]); //Samuel
-//los objetos pueden ser modificados, son "mutables". Podemos acceder (leer) el valor de sus propiedades usando . o [] y también asignarles un nuevo valor con el operador de asignación =.
